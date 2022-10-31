b08d8d166580dd40302d97a2e847a6bc893ceab6
RustamGadzhiev29 <changethe@mail.ru>
2022-10-31 18:59:52 +0300

    Updates

---
+++ precache-manifest.bdced3b5dee3fc51972392116895bab0.js
@@ -0,0 +1,326 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee2908b3a2fae32028d9482ed3916cf",
+    "url": "/ipad-demo/index.html"
+  },
+  {
+    "revision": "e9e2bad27049a8f13f94",
+    "url": "/ipad-demo/static/css/main.9146ffab.chunk.css"
+  },
+  {
+    "revision": "57e024720918be895af4",
+    "url": "/ipad-demo/static/js/2.5b6549e8.chunk.js"
+  },
+  {
+    "revision": "df0557c1fa2ab91a9f63d934edc4d1cb",
+    "url": "/ipad-demo/static/js/2.5b6549e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9e2bad27049a8f13f94",
+    "url": "/ipad-demo/static/js/main.c2d9dd28.chunk.js"
+  },
+  {
+    "revision": "ce97876817853ec2dc9d",
+    "url": "/ipad-demo/static/js/runtime-main.87715ff7.js"
+  },
+  {
+    "revision": "6b02fd51cf66b10a7259f5d8a7b1c2cf",
+    "url": "/ipad-demo/static/media/8bg.6b02fd51.jpg"
+  },
+  {
+    "revision": "a494c0ee797e5dc19fb3b85e8d9391c1",
+    "url": "/ipad-demo/static/media/AppStore.a494c0ee.png"
+  },
+  {
+    "revision": "c25de8b2e468a0c1dd0249b50a585933",
+    "url": "/ipad-demo/static/media/Applicons.c25de8b2.png"
+  },
+  {
+    "revision": "8d37f108bc4224339d7fa77320bbdfbf",
+    "url": "/ipad-demo/static/media/Burnable.8d37f108.png"
+  },
+  {
+    "revision": "4864e96ae50a98618d69327e02731c39",
+    "url": "/ipad-demo/static/media/Calculator.4864e96a.png"
+  },
+  {
+    "revision": "18c533b114f814b75eff46a1f63a56d7",
+    "url": "/ipad-demo/static/media/Calendar.18c533b1.png"
+  },
+  {
+    "revision": "3cae51a96a0fa440fce4ec597e5f0f79",
+    "url": "/ipad-demo/static/media/CalendarBlank.3cae51a9.png"
+  },
+  {
+    "revision": "791708bdff075ff11919c4beae30e89a",
+    "url": "/ipad-demo/static/media/CalendarOfficial.791708bd.png"
+  },
+  {
+    "revision": "d46bfe9783a46766b0a6cc3e9490fef5",
+    "url": "/ipad-demo/static/media/Camera.d46bfe97.png"
+  },
+  {
+    "revision": "8d2116da8e7334dd23cfc3daeebd798a",
+    "url": "/ipad-demo/static/media/CandyBar.8d2116da.png"
+  },
+  {
+    "revision": "6d86d99158436c6763a09e5d5f006e9b",
+    "url": "/ipad-demo/static/media/Clock.6d86d991.png"
+  },
+  {
+    "revision": "f7dd29e3fbcde42e2fe4bddb95be71f6",
+    "url": "/ipad-demo/static/media/CloudApp.f7dd29e3.png"
+  },
+  {
+    "revision": "5e330a999e3cedd1515193522ac12f8f",
+    "url": "/ipad-demo/static/media/Cobook.5e330a99.png"
+  },
+  {
+    "revision": "f79775a339ee86f3f365c2cf1183eb6e",
+    "url": "/ipad-demo/static/media/Compass.f79775a3.png"
+  },
+  {
+    "revision": "cd8b7e58ab2883fcb4852f942d4f9978",
+    "url": "/ipad-demo/static/media/Contacts.cd8b7e58.png"
+  },
+  {
+    "revision": "6e971315f90a33a7606b2c3ce552f386",
+    "url": "/ipad-demo/static/media/DesignBriefs.6e971315.png"
+  },
+  {
+    "revision": "03453366b567081e608f955af2690b15",
+    "url": "/ipad-demo/static/media/Developer.03453366.png"
+  },
+  {
+    "revision": "f1fea6309ec78e94ee5a91c4ac94d031",
+    "url": "/ipad-demo/static/media/Dictionary.f1fea630.png"
+  },
+  {
+    "revision": "f0410c66eb26b045ecb8f24313a19aa3",
+    "url": "/ipad-demo/static/media/Doo.f0410c66.png"
+  },
+  {
+    "revision": "6871939e640e6c13f73bf8f383cfc5fd",
+    "url": "/ipad-demo/static/media/Downloads.6871939e.png"
+  },
+  {
+    "revision": "3cbe18a1c5f4a4e4286a55797ed7c808",
+    "url": "/ipad-demo/static/media/DropStuff.3cbe18a1.png"
+  },
+  {
+    "revision": "09a88703e1ebab6e5ca59e70c7011573",
+    "url": "/ipad-demo/static/media/EasyFind.09a88703.png"
+  },
+  {
+    "revision": "6ecaf76fa2f4053648fcc201c9ad829e",
+    "url": "/ipad-demo/static/media/FaceTime.6ecaf76f.png"
+  },
+  {
+    "revision": "1cf463f1a83c25ada0b4ab3a2b0d437d",
+    "url": "/ipad-demo/static/media/Finder.1cf463f1.png"
+  },
+  {
+    "revision": "91c36f7b77c2ed850726d14eb327d1b0",
+    "url": "/ipad-demo/static/media/FontExplorerX.91c36f7b.png"
+  },
+  {
+    "revision": "ce7000f983c76b30c2843163e7eb8593",
+    "url": "/ipad-demo/static/media/GameCentealt2.ce7000f9.png"
+  },
+  {
+    "revision": "afde550fd6fa482cfae1440875c1f510",
+    "url": "/ipad-demo/static/media/GameCenteralt3.afde550f.png"
+  },
+  {
+    "revision": "f2048a4a1a792cf47f76d2d189f5c8c5",
+    "url": "/ipad-demo/static/media/GameCenteralt4.f2048a4a.png"
+  },
+  {
+    "revision": "452fa8cc53cf7fa1530ecc40e1888221",
+    "url": "/ipad-demo/static/media/Gemini.452fa8cc.png"
+  },
+  {
+    "revision": "5479875a8050af89373b393767bcfcc6",
+    "url": "/ipad-demo/static/media/IdeaStuff.5479875a.png"
+  },
+  {
+    "revision": "811c7bd958514e4a17b97c41c6707215",
+    "url": "/ipad-demo/static/media/IdeaStuff2.811c7bd9.png"
+  },
+  {
+    "revision": "13e82454c953cfd56994a759842bbb6b",
+    "url": "/ipad-demo/static/media/Illustrator.13e82454.png"
+  },
+  {
+    "revision": "80e8a26eea28068d19aa5923cf5a29f3",
+    "url": "/ipad-demo/static/media/InDesign.80e8a26e.png"
+  },
+  {
+    "revision": "6a8192b09214605d51ffb0c6c70a8c0e",
+    "url": "/ipad-demo/static/media/Internal.6a8192b0.png"
+  },
+  {
+    "revision": "ba7299dd8443736d162f367cdaea5f77",
+    "url": "/ipad-demo/static/media/LaCieFAPorsche.ba7299dd.png"
+  },
+  {
+    "revision": "c888e138f5c813c9a36a9c24cad8fd2f",
+    "url": "/ipad-demo/static/media/LaCieFAPorsche2.c888e138.png"
+  },
+  {
+    "revision": "9f343dd787c1c0903b23a6d7a89e7c44",
+    "url": "/ipad-demo/static/media/LaCieNeilPoulton.9f343dd7.png"
+  },
+  {
+    "revision": "e17acdfb98f0c67d91d9330a6c71bb05",
+    "url": "/ipad-demo/static/media/LaCieRugged.e17acdfb.png"
+  },
+  {
+    "revision": "6922310911c20c2568f34d73be0ee149",
+    "url": "/ipad-demo/static/media/LaCieRugged2.69223109.png"
+  },
+  {
+    "revision": "cb5fef3b0536e38984895344ee6ae89a",
+    "url": "/ipad-demo/static/media/Library.cb5fef3b.png"
+  },
+  {
+    "revision": "270ce8c61af20aae281e1e161c8cf163",
+    "url": "/ipad-demo/static/media/Mail.270ce8c6.png"
+  },
+  {
+    "revision": "5334bc22cb0b461d7c975a677f5d6629",
+    "url": "/ipad-demo/static/media/Maps.5334bc22.png"
+  },
+  {
+    "revision": "7b7bc658d3fce83780679e84dc62f2fa",
+    "url": "/ipad-demo/static/media/Messages.7b7bc658.png"
+  },
+  {
+    "revision": "505e45c9e8d3ce3257a3a46c7b273207",
+    "url": "/ipad-demo/static/media/MiscDesigns01.505e45c9.png"
+  },
+  {
+    "revision": "acd71cdde70bf48469007d32ce3301cc",
+    "url": "/ipad-demo/static/media/Monosnap.acd71cdd.png"
+  },
+  {
+    "revision": "e99f649c64da2dc78174d623b91f9f62",
+    "url": "/ipad-demo/static/media/Music.e99f649c.png"
+  },
+  {
+    "revision": "f12c4751fdbba462a66b41a18cc87036",
+    "url": "/ipad-demo/static/media/Newsstand.f12c4751.png"
+  },
+  {
+    "revision": "e56ca97f59c13584563f36a4054b167c",
+    "url": "/ipad-demo/static/media/Notes.e56ca97f.png"
+  },
+  {
+    "revision": "6431f84dd09f7f58a57116f5f57b0348",
+    "url": "/ipad-demo/static/media/Passbook.6431f84d.png"
+  },
+  {
+    "revision": "3dd0ae0ad52163b81b903c4bed5f2e9a",
+    "url": "/ipad-demo/static/media/Phone.3dd0ae0a.png"
+  },
+  {
+    "revision": "dee722e5e0bdd6950d754f74a8a49065",
+    "url": "/ipad-demo/static/media/Photos.dee722e5.png"
+  },
+  {
+    "revision": "1b6c75dd25256f1a7ae05654429f6010",
+    "url": "/ipad-demo/static/media/Photoshop.1b6c75dd.png"
+  },
+  {
+    "revision": "492bdd9ff0f32694c87536762f62ff1f",
+    "url": "/ipad-demo/static/media/Preview.492bdd9f.png"
+  },
+  {
+    "revision": "b2237b2833407b80c046f4f969346631",
+    "url": "/ipad-demo/static/media/RSSNotifier.b2237b28.png"
+  },
+  {
+    "revision": "72ea372ef71958b37767601a9f6473b0",
+    "url": "/ipad-demo/static/media/Reminders.72ea372e.png"
+  },
+  {
+    "revision": "0eab5d7996aff98808a6ddfb7c4b12ed",
+    "url": "/ipad-demo/static/media/Safari.0eab5d79.png"
+  },
+  {
+    "revision": "73ace9f6e1e78fc09f5043356c06d28a",
+    "url": "/ipad-demo/static/media/Settings.73ace9f6.png"
+  },
+  {
+    "revision": "e1d924335d26099ab51e12984a31f635",
+    "url": "/ipad-demo/static/media/Smart.e1d92433.png"
+  },
+  {
+    "revision": "13312504ffcf741c616e41c18f2ac57a",
+    "url": "/ipad-demo/static/media/Stocks.13312504.png"
+  },
+  {
+    "revision": "b99d070a2e001322ae87049afe249429",
+    "url": "/ipad-demo/static/media/System.b99d070a.png"
+  },
+  {
+    "revision": "558cbd0e27eb2249fb21a783115290a8",
+    "url": "/ipad-demo/static/media/TimeMachine.558cbd0e.png"
+  },
+  {
+    "revision": "8af365be2ca2c4ce89a2009695adadf3",
+    "url": "/ipad-demo/static/media/TimeMachineDisk.8af365be.png"
+  },
+  {
+    "revision": "511ff2beb1e0b6c01f5951c8cf3daa9b",
+    "url": "/ipad-demo/static/media/TrashEmpty.511ff2be.png"
+  },
+  {
+    "revision": "3f0e7d463d447ea9b5e494511d257109",
+    "url": "/ipad-demo/static/media/TrashFull.3f0e7d46.png"
+  },
+  {
+    "revision": "f49d3bf1b774941c3fd610cee4fa64ad",
+    "url": "/ipad-demo/static/media/TrashManaged.f49d3bf1.png"
+  },
+  {
+    "revision": "0452ac38302c16481666342d3d532c96",
+    "url": "/ipad-demo/static/media/Twitter.0452ac38.png"
+  },
+  {
+    "revision": "e64bd80274754124d3456ab45c8b8352",
+    "url": "/ipad-demo/static/media/Users.e64bd802.png"
+  },
+  {
+    "revision": "643d786b1cf02f8ff7becfa0376daee4",
+    "url": "/ipad-demo/static/media/Videos.643d786b.png"
+  },
+  {
+    "revision": "c166edb65839e9a3de349763598db6e1",
+    "url": "/ipad-demo/static/media/Weather.c166edb6.png"
+  },
+  {
+    "revision": "bd8debf5bf2d65581b207806d621036f",
+    "url": "/ipad-demo/static/media/background.bd8debf5.jpg"
+  },
+  {
+    "revision": "46ba99ac676d6c9820a85400da3adf6f",
+    "url": "/ipad-demo/static/media/iTunesStore.46ba99ac.png"
+  },
+  {
+    "revision": "f2ac16a030481654b42c885c06a86766",
+    "url": "/ipad-demo/static/media/ipad-mini-horizontal.f2ac16a0.png"
+  },
+  {
+    "revision": "68da61b416a0ddc1b02535414e6f3666",
+    "url": "/ipad-demo/static/media/ipad-mini-vertifical.68da61b4.png"
+  },
+  {
+    "revision": "7b42a17d392652c95ff45d4b50f59b4e",
+    "url": "/ipad-demo/static/media/iphone5-horizontal.7b42a17d.png"
+  },
+  {
+    "revision": "3e32c80a856a0d797a57a1c16c5e2e53",
+    "url": "/ipad-demo/static/media/iphone5-vertical.3e32c80a.png"
+  }
+]);
